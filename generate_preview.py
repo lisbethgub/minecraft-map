f799f3ced9099d90a10690044bcd8dd267ff50dc
Елизавета Шаповалова <shapovalovae95@gmail.com>
2025-05-02 01:21:08 +0400

    add files for generation map preview

---
+++ generate_preview.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+import time
+import os
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page(viewport={"width": 1000, "height": 800})
+    page.goto(f"file://{os.getcwd()}/index.html")
+    time.sleep(2)  # ждём, пока карта прогрузится
+    page.screenshot(path="preview.png")
+    browser.close()
