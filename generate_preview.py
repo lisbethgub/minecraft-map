4b7542f600738ee3a0cf95f3cd2d06cd9c848108
Елизавета Шаповалова <shapovalovae95@gmail.com>
2025-07-01 01:57:13 +0400

    Refactoring code

--- generate_preview.py
+++ generate_preview.py
@@ -6,6 +6,6 @@ with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page(viewport={"width": 1600, "height": 800})
     page.goto(f"file://{os.getcwd()}/index.html")
-    time.sleep(10)  # ждём, пока карта прогрузится
+    time.sleep(2)  # ждём, пока карта прогрузится
     page.screenshot(path="preview.png")
     browser.close()
